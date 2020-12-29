022d60fc08a4dcf2a038690f87415fe00544bf48
Ivan M <im4adm@gmail.com>
2020-12-29 21:18:53 +0300

    pre-9

--- confi-2/IIS_sync.ps1
+++ confi-2/IIS_sync.ps1
@@ -4,21 +4,21 @@ $out_paths = "./"
 
 git config user.email "Jankins@mail.ru"
 git config user.name "Jan Kins"
-git checkout master
-
-foreach ($p in $in_paths) {
-    Copy-Item -Path $p -Destination $out_paths  -Recurse -Force
-}
-
+# git checkout master
+#
+#foreach ($p in $in_paths) {
+#    Copy-Item -Path $p -Destination $out_paths  -Recurse -Force
+#}
+#
 $out = (git status) | Out-String
 Write-Output "GITI = $out"
 
-if ( $out.Contains("nothing to commit") ) {
-    echo " => Config has not changed!"
-}
-else {
-    git add .
-    git commit -m "Config update with Jankins Job"
-    git push origin master
-    echo " => Config in Repo was updated!"
-}
+#if ( $out.Contains("nothing to commit") ) {
+#    echo " => Config has not changed!"
+#}
+#else {
+#    git add .
+#    git commit -m "Config update with Jankins Job"
+#    git push origin master
+#    echo " => Config in Repo was updated!"
+#}
