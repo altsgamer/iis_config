dce8107f293d19f0bd83ec7993c17f7de3681ba3
Ivan M <im4adm@gmail.com>
2020-12-29 19:14:22 +0300

    156

--- confi-2/IIS_sync.ps1
+++ confi-2/IIS_sync.ps1
@@ -1,6 +1,19 @@
 
-echo " => Hi from Jenk!"
+$in_paths = @("/home/xt1/*","/home/xt2/*")
+$out_paths = "./"
+
+foreach ($p in $in_paths) {
+    Copy-Item -Path $p -Destination $out_paths  -Recurse -Force
+}
 
 $out = (git status) | Out-String
 
-echo " => this is OUT: $out"
+if ( $out.Contains("nothing to commit") ) {
+    echo " => Config has not changed!"
+}
+else {
+    git add .
+    git commit -m "Config update"
+    git push -u origin master
+    echo " => Config in Repo was updated!"
+}
